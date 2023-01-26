fdeb5d83c7b4a7388a1d0538a1b223733765f701
TommyHanle <tomboehan@gmail.com>
2023-01-26 14:23:50 -0800

    deploy

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const passport = require('passport');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/login');
 });
@@ -18,7 +17,7 @@ router.get('/oauth2callback', passport.authenticate(
   'google',
   {
     successRedirect: '/trips',
-    failureRedirect: '/login'
+    failureRedirect: '/'
   }
 ));
 
