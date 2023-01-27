aa536e945b8eb105b768ce82fecb676e3bce18e2
TommyHanle <tomboehan@gmail.com>
2023-01-26 16:46:11 -0800

    minor improvements

--- controllers/login.js
+++ controllers/login.js
@@ -2,14 +2,10 @@ module.exports = {
   index,
 }
 
-/* function index(req, res) {
-      res.render('login/index', { title: 'Login' });
-} */ 
-
 function index(req, res) {
   if (req.user) {
     res.redirect('/trips');
   } else {
-    res.render('login/index', { title: '' });
+    res.render('login', { title: '' });
   }
 }
